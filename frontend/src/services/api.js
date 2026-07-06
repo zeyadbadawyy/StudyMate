ef80cf3480e9a5957ead1e7d123e29bb7f9ccb36
Zeyad Badawy <zeyadbadawyy@gmail.com>
2026-07-06 19:45:48 +0300

    Configure production backend URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://studymate-production-2c5e.up.railway.app",
 });
 
 export default api;
